1e70a5eb34b94c8b642118d992035cf880c40b03
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-03-27 22:02:28 +0530

    2888-MINIMUM-INDEX-OF-A-VALID-SPLIT Stats: Time: 11 ms (95.51%), Space: 63.9 MB (10.26%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,23 +1,27 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        //get the dominnant elelment
+        //get the dominnant elelment//O(N)
         int n= nums.size();
-        Integer [] li = new Integer[n];
-        int j=0;
+        int dom=nums.get(0), cnt=0;
         for (Integer i: nums){
-            li[j] = i;
-            j++;
+            if (i==dom)
+            cnt++;
+            else
+            cnt--;
+            if (cnt==0){
+            dom=i;
+            cnt++;
+            }
         }
-        Arrays.sort(li);
-        int dom= li[n/2];
         //check if it's possible to make split 
         int count=0;
-        for (Integer i: li){
+        for (Integer i: nums){
             if (i==dom)
             count++;
         }
         if (count < (n/2 +1+n%2))
         return -1;
+        // making split
         int like=0, unlike=0, ans=0;
         for (Integer i: nums){
             if (i==dom)
