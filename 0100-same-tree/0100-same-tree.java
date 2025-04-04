218a671d2bfe2b7c74cc267785a02a83c02fec9e
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-04-05 02:59:38 +0530

    0100-SAME-TREE Stats: Time: 0 ms (100%), Space: 41.6 MB (6.2%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,9 +15,9 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p==null)
-            return (q==null)?true:false;
-        if (q==null && p!=null)
+        if (p==null && q==null)
+            return true;
+        if (q==null || p==null)
         return false;
         if (p.val!=q.val)
         return false;
