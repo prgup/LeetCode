27666c03e43c86fa22d3ac572619f57878ff66d5
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-04-08 01:41:41 +0530

    0023-MERGE-K-SORTED-LISTS Stats: Time: 144 ms (5.31%), Space: 44.3 MB (90.22%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -11,29 +11,29 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         int n = lists.length;
-        int j=n;
+        int j = n;
         ListNode ans = new ListNode(0);
         ListNode head = ans;
-        while (j>0){
-            int idx = -1, min=Integer.MAX_VALUE; 
-            for (int i=0; i<n;i++){
-                if (lists[i]!=null){
-                if ( min> lists[i].val){
-                    idx=i;
-                    min=lists[i].val;
-                }
+        while (j > 0) {
+            int idx = -1, min = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                if (lists[i] != null) {
+                    if (min > lists[i].val) {
+                        idx = i;
+                        min = lists[i].val;
+                    }
 
-                }}
-            if (idx!=-1){
-            ans.next = new ListNode(min);
-            ans=ans.next;
-            lists[idx]=lists[idx].next;
-            if(lists[idx]==null)
-            j--;
+                }
             }
-            else
-            j=0;
+            if (idx != -1) {
+                ans.next = lists[idx];
+                ans = ans.next;
+                lists[idx] = lists[idx].next;
+                if (lists[idx] == null)
+                    j--;
+            } else
+                j = 0;
         }
-     return head.next;   
+        return head.next;
     }
 }
