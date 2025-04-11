82c52eb5bdd1125f18262065b44680abb0976a59
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-04-12 00:36:07 +0530

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 186 ms (27.31%), Space: 42.3 MB (74.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,50 +1,29 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n=s.length();
-        
-        int ans=1, l=0, r=0;
-        for (int i=0;i<n; i++){
-            boolean even = true, odd = true;
-            int l1=i,l2=i-1, r1=i+1,r2=i+1;
-               
-            
-            while (even || odd){
-                if (l1<0 || r1>=n)
-                        even=false;  
-             if (l2<0 || r2>=n)
-                        odd=false;
-                if (even){
-                    if (s.charAt(l1)==s.charAt(r1)){
-                        if (ans <r1-l1+1){
-                            r=r1;
-                            l=l1;
-                            ans=r1-l1+1;
-                        }
-                        l1--;
-                        r1++;
-                    }
-                    else 
-                        even=false;
-                   
-                }
-                if (odd){
-                     if (s.charAt(l2)==s.charAt(r2)){
-                         if (ans <r2-l2+1){
-                            r=r2;
-                            l=l2;
-                            ans=r2-l2+1;
-                        }
-                        l2--;
-                        r2++;
-                    }
-                    else 
-                        odd=false;
-                   
+        for (int length = s.length(); length > 0; length--) {
+            for (int start = 0; start <= s.length() - length; start++) {
+                if (check(start, start + length, s)) {
+                    return s.substring(start, start + length);
                 }
             }
         }
-        // System.out.println(l+" "+r);
-        return s.substring(l,r+1);
-        
+
+        return "";
+    }
+
+    private boolean check(int i, int j, String s) {
+        int left = i;
+        int right = j - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
