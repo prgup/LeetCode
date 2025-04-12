be4941ab7c558d57092e57fcc0a609cd8a4c22e2
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-04-12 19:17:49 +0530

    0078-SUBSETS Stats: Time: 1 ms (52.23%), Space: 42.8 MB (65.16%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,31 +1,21 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        System.out.println("1"+ ans.toString());
-        ans.add(new ArrayList<>());
-        System.out.println("2"+ans.toString());
+        ans.add(new ArrayList<>()); // start with empty subset
         generate(nums, 0, ans);
-        System.out.println("8"+ans.toString());
         return ans;
     }
-    void generate (int [] nums, int idx, List<List<Integer>> temp ){
-        if  ( idx == nums.length){
-            System.out.println("9"+temp.toString());
-                    return;
+
+    void generate(int[] nums, int idx, List<List<Integer>> ans) {
+        if (idx == nums.length) return;
+
+        int size = ans.size();
+        for (int i = 0; i < size; i++) {
+            List<Integer> newSubset = new ArrayList<>(ans.get(i));
+            newSubset.add(nums[idx]);
+            ans.add(newSubset);
         }
-        List<List<Integer>> ans = new ArrayList<>();
-        System.out.println("3" + ans.toString());
-        for (List<Integer> li : temp){
-            ans.add(new ArrayList<>(li));
-            System.out.println("4"+ ans.toString());
-            li.add(nums[idx]);
-            ans.add(new ArrayList<>(li));
-            System.out.println("5"+ans.toString());
-        }
-        System.out.println("6"+temp.toString());
-        temp.clear();        
-        temp.addAll(ans);
-        System.out.println("7"+temp.toString());
-        generate (nums, idx+1, temp);
+
+        generate(nums, idx + 1, ans);
     }
 }
