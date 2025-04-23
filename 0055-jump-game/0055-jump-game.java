770fafed484aa01907fc5a0f0214ef95a007188b
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-04-24 02:06:15 +0530

    0055-JUMP-GAME Stats: Time: 275 ms (9.16%), Space: 46 MB (21.68%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        
-        // int idx = nums[0];
-        int max = nums[0];
-        // int j=1;
-        for (int i=1; i<nums.length; i++){
-            if (max < i)
-            return false;
-            max = Math.max(max, nums[i] + i);
+        int n = nums.length;
+        int [] reach = new int[n];
+        reach[n-1]=1;
+        for (int i=nums.length-2; i >= 0; i--){
+            int j= nums[i];
+            int k= i, l= Math.min(n-1, i+j);
+            while (k < l){
+                reach[k]=reach[l];
+                k++;
+            }
         }
-        return true;
+        return reach[0]==1?true:false;
     }
 }
