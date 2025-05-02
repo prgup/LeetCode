dc1c7cd09a320a8e198d6ef8c962ab6c0647baf0
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-05-03 03:07:45 +0530

    0137-SINGLE-NUMBER-II Stats: Time: 1 ms (82.29%), Space: 45.2 MB (96.68%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int one=0, two=0, three=0;
-        for (int i: nums){
+        for (int i:nums){
             two|=(one&i);
             one^=i;
             three=one&two;
@@ -9,6 +9,5 @@ class Solution {
             two&=(~three);
         }
         return one;
-        
     }
 }
