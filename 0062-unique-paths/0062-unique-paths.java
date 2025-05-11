37160b766315dd76b7c138179413e0e53a8da3be
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-05-11 14:39:38 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 40.9 MB (11.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,21 +2,17 @@ class Solution {
     
     public int uniquePaths(int m, int n) {
         int [][] memoization = new int[m+1][n+1];
-        int [][] visited = new int[m+1][n+1];
-        visited[1][1]=1;
         memoization[1][1]=1;
-        return paths(m,n, memoization, visited);
+        return paths(m,n, memoization);
     }
-    int paths (int m, int n, int [][] memoization, int [][] visited){
-        if (visited[m][n]==1)
+    int paths (int m, int n, int [][] memoization){
+        if (memoization[m][n] !=0)
         return memoization[m][n];
-        if ((m>1) && visited[m-1][n]!=1){
-                paths(m-1,n, memoization, visited);
+        if (m>1){
+                paths(m-1,n, memoization);
             }
-        if ((n>1) && visited[m][n-1]!=1){
-                paths(m,n-1, memoization, visited);
-        }
-        visited[m][n]=1;
+        if (n>1)
+                paths(m,n-1, memoization);
         return memoization[m][n]=memoization[m-1][n]+memoization[m][n-1];
 
     }
