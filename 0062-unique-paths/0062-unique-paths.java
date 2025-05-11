d4a87f669061e81e7736a5fa847457ccb1a90d71
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-05-11 18:43:37 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 40.1 MB (97.74%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,10 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        //At any cell (i, j), you only need:
-        //dp[j] â number of paths from the cell below (i+1, j)
-        //dp[j + 1] â number of paths from the cell to the right (i, j+1)
 
-
-        int [] dp = new int[n];
-        dp[n-1]=1;
-
-        for (int i=m-1; i>=0; i--){
-            for (int j=n-2; j>=0; j--){
-                dp[j] =dp[j]+dp[j+1];
-            }
+        long res = 1;
+        for (int i = 1; i <= m - 1; i++) {
+            res = res * (n - 1 + i) / i;
         }
-        return  dp[0];
+        return (int) res;
     }
 }
