8d519eaa8adef55d4c0b421b99a0120b98b3ef8c
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-05-23 01:02:26 +0530

    0190-REVERSE-BITS Stats: Time: 1 ms (14.46%), Space: 42 MB (34.1%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,17 +1,30 @@
 public class Solution {
+    private static final int[] reverseTable = new int[256];
+
+    static {
+        for (int i = 0; i < 256; i++) {
+            reverseTable[i] = reverseByte(i);
+        }
+    }
+
+    private static int reverseByte(int b) {
+        int res = 0;
+        for (int i = 0; i < 8; i++) {
+            res <<= 1;
+            res |= (b & 1);
+            b >>= 1;
+        }
+        return res;
+    }
+
     public int reverseBits(int n) {
-        int count =0;
         int ans=0;
-        while ( count <32){
-            ans<<=1;
-            // if ((n&1)==1){
-            //     ans+=1;
-            // }
-            ans|=(n&1);
-            n>>>=1;
-            count++;
+        for (int i=0; i<4; i++){
+            int bit = n&255;
+            ans<<=8;
+            ans|=reverseTable[bit];
+            n>>=8;
         }
         return ans;
     }
-
 }
