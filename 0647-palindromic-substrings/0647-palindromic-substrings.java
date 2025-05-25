88259eb3ca1d329d3cbb1a22473c4a7dd091b85e
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-05-26 04:11:46 +0530

    0647-PALINDROMIC-SUBSTRINGS Stats: Time: 5 ms (97.43%), Space: 41.6 MB (74.17%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,32 +1,29 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n=s.length();
-        int ans=0; 
+        int n = s.length();
+        int cnt=n;
         for (int i=0; i<n; i++){
-            //for substring with i as middle
-            int left =i;
-            int right = i;
-            while  (-1<left && n>right){
-                if (s.charAt(left)!=s.charAt(right))
-                break;
-                ans++;
-                left--;
-                right++;
-            }
-             //for substring with i and i+1 as middle
-            left =i;
-            right = i+1;
-            while  (-1<left && n>right){
-                if (s.charAt(left)!=s.charAt(right))
-                break;
-                ans++;
-                left--;
-                right++;
-            }
+            //for odd string with i as middle
+            cnt+=palidromeCheck(s, i-1, i+1);
+            //for even string with i & i+1 as middle
+            if ((i <n-1) && s.charAt(i)==s.charAt(i+1))
+            cnt+= (1+palidromeCheck (s, i-1,i+2 ));
+        }
+        return cnt;
+        
+    }
+    private int palidromeCheck (String s, int left, int right){
+        int n = s.length();
+        int ans=0;
+        if (0>left || right>=n || left>=right)
+        return ans;
+        while (-1<left && right<n){
+            if (s.charAt(left)!=s.charAt(right))
+            return ans;
+            left--;
+            right++;
+            ans++;
         }
         return ans;
-
-
-        
     }
 }
