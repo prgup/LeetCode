e4924356f70f07530b3ae85fabf237a475f3c317
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-05-26 03:06:53 +0530

    0211-DESIGN-ADD-AND-SEARCH-WORDS-DATA-STRUCTURE Stats: Time: 178 ms (67.85%), Space: 103.4 MB (29.93%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,63 +1,50 @@
 class WordDictionary {
-    class TrieNode {
-        TrieNode [] arr;
+    class Node {
+        Node [] child;
         boolean isEnd;
 
-        public TrieNode (){
-            arr = new TrieNode [26];
-            isEnd=false;
+        public Node (){
+            child = new Node[26];
+            isEnd = false;
         }
     }
 
-    TrieNode root;
+    Node root;
 
     public WordDictionary() {
-        root = new TrieNode();
+        root = new Node();
     }
     
     public void addWord(String word) {
-        TrieNode temp = root;
-        for (char i : word.toCharArray()){
-            if (temp.arr[i-'a']==null)
-                temp.arr[i-'a']= new TrieNode();
-            temp=temp.arr[i-97];
+        Node temp = root;
+        int n = word.length();
+        for (int i=0; i<n;i++){
+            if (temp.child[word.charAt(i)-'a']==null)
+            temp.child[word.charAt(i)-'a']= new Node();
+            temp= temp.child[word.charAt(i)-'a'];
         }
         temp.isEnd=true;
         
     }
-    public boolean search(String word, TrieNode temp){
-        int n = word.length();
-        for (int i=0; i<n; i++){
+    public boolean search(String word, Node temp, int idx){
+        for (int i=idx; i<word.length(); i++){
             if (word.charAt(i)=='.'){
-                for (int j=0;j<26; j++){
-                    if (temp.arr[j]!=null && search(word.substring(i+1,n), temp.arr[j]))
-                    return true; 
+                for (int j=0; j<26; j++){
+                    if ((temp.child[j]!=null) && search(word, temp.child[j], i+1))
+                    return true;
                 }
                 return false;
-                // return search(word.substring(i+1,n), temp);
             }
-            if (temp.arr[word.charAt(i)-'a']==null)
-            return false;
-            temp=temp.arr[word.charAt(i)-97];
+            
+            if (temp.child[word.charAt(i)-'a']==null)
+                return false;
+            temp= temp.child[word.charAt(i)-'a'];
         }
         return temp.isEnd;
-
     }
     
     public boolean search(String word) {
-        TrieNode temp = root;
-        int n = word.length();
-        return search(word, temp);
-        // for (int i=0; i<n; i++){
-        //     if (word.charAt(i)=='.'){
-        //         return search(word.substring(i+1,n));
-        //     }
-        //     if (temp.arr[word.charAt(i)-'a']==null)
-        //     return false;
-        //     temp=temp.arr[word.charAt(i)-'a'];
-        // }
-        // return temp.isEnd;
-        
+        return search(word, root, 0);
         
     }
 }
