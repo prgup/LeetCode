101897c572f8bd48ee96e83891d0410473b8c7e9
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-06-05 01:57:19 +0530

    0417-PACIFIC-ATLANTIC-WATER-FLOW Stats: Time: 5 ms (61.85%), Space: 45.6 MB (60.13%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,49 +1,37 @@
 class Solution {
-    public List<List<Integer>> pacificAtlantic(int[][] height) {
-        int row = height.length, col = height[0].length;
-        int[][] pacific = new int[height.length][height[0].length];
-        int[][] atlantic = new int[height.length][height[0].length];
-        for (int i = 0; i < row; i++) {
-            pacific[i][0] = 1;
-            atlantic[row - i - 1][col - 1] = 1;
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int row = heights.length, col = heights[0].length;
+        boolean [][] flowPac = new boolean[row][col];
+        boolean [][] flowAt = new boolean[row][col];
+        for (int i=0; i<row; i++){
+            dfs(flowPac, heights, i, 0, row, col);
+            dfs(flowAt, heights, i, col-1, row, col);
         }
-        for (int j = 0; j < col; j++) {
-            pacific[0][j] = 1;
-            atlantic[row - 1][j] = 1;
-        }
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (pacific[i][j] == 1)
-                    dfs(i, j, height, pacific);
-                if (atlantic[i][j] == 1)
-                    dfs(i, j, height, atlantic);
-            }
+        for (int i=0; i<col; i++){
+            dfs(flowPac, heights, 0, i, row, col);
+            dfs(flowAt, heights, row-1, i, row, col);
         }
         List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (pacific[i][j] == 1 && atlantic[i][j] == 1) {
-                    List<Integer> li = new ArrayList<>();
-                    li.add(i);
-                    li.add(j);
-                    ans.add(li);
+        for (int i=0; i<row; i++){
+            for (int j=0; j<col; j++){
+                if (flowPac[i][j] && flowAt[i][j]){
+                    ans.add(Arrays.asList(i,j));
                 }
-            }
-        }
+            }}
         return ans;
+
     }
 
-    public void dfs(int i, int j, int[][] height, int[][] flow) {
-
-        for (int[] temp : new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } }) {
-            int i1 = i + temp[0];
-            int j1 = j + temp[1];
-            if (i1 < 0 || j1 < 0 || i1 >= height.length || j1 >= height[0].length || flow[i1][j1] == 1)
+    private void dfs (boolean [][] flow, int [][] heights, int i, int j, int row, int col){
+        flow[i][j]=true;
+        for (int [] li : new int [][] {{-1,0}, {1,0},{0,-1},{0,1}}){
+            int a = i+li[0];
+            int b= j+li[1];
+            if (a<0 || a>=row || b<0 || b>=col)
                 continue;
-            if (height[i1][j1] >= height[i][j]) {
-                flow[i1][j1] = 1;
-                dfs(i1, j1, height, flow);
+            else if (flow[a][b] || heights[i][j] > heights[a][b])
+            continue;
+            dfs(flow, heights, a, b, row, col);
             }
-        }
     }
 }
