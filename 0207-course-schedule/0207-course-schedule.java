095e2a5c463bd99d8ce9e0755d09edb2a35bc138
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-06-06 17:41:59 +0530

    0207-COURSE-SCHEDULE Stats: Time: 29 ms (5.99%), Space: 46.3 MB (6.86%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -3,15 +3,15 @@ class Solution {
         Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int i=0; i<pre.length; i++){
             int x = pre[i][0], y = pre[i][1];
-            List<Integer> li = graph.getOrDefault(y, new ArrayList<>());
-            li.add(x);
-            graph.put(y, li);
+            // List<Integer> li = graph.getOrDefault(y, new ArrayList<>());
+            // li.add(x);
+            // graph.put(y, li);
+            graph.computeIfAbsent(y, k -> new ArrayList<>()).add(x);
         }
         int [] visited = new int[numC];
         for (int i=0; i<numC; i++){
             if (visited[i]==1)
             continue;
-            // visied[i]
             boolean status = dfs(graph, visited, i);
             if (!status)
             return false;
