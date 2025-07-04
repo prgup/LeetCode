073d0bef288bd0c2db27c80e78709328af7240fd
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-07-05 03:49:43 +0530

    0015-3SUM Stats: Time: 30 ms (69.39%), Space: 52.8 MB (16.74%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,54 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+
+        Arrays.sort(nums);
         int n = nums.length;
-        int [] li = Arrays.copyOf(nums, n);//n//n
-        Arrays.sort(li);//nlongn
-        List <List<Integer>> temp = new ArrayList<>();//n
-        for (int i=0; i<n-2; i++){
-            int cur = li[i];
-            if (i >0 && li[i]==li[i-1])
-            continue;
-            if (cur>0)
-            break;
-            int left = i+1, right = n-1;
-            while (left<right){
-                if ((cur + li[left]+li[right]) > 0)
-                right--;
-                else if ((cur + li[left]+li[right]) < 0)
-                left++;
-                else {
-                    List<Integer> t = new ArrayList<>();
-                    t.add(cur);
-                    t.add(li[left]);
-                    t.add(li[right]);
-                    temp.add(t);
-                    while (left < right && li[left] ==li[left+1]){
-                    left++;
-                    }
-                    while (left < right && li[right]==li[right-1]){
+        if (nums[n - 1] < 0 || nums[0] > 0)
+            return new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i - 1] == nums[i])
+                continue;
+            int right = n - 1;
+            int left = i + 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[left]);
+                    temp.add(nums[right]);
+                    ans.add(temp);
+                    int current = nums[left];
+                    while (nums[left] == current && left < n - 1)
+                        left++;
+                    while (left < right && nums[right] == nums[right -1]) 
                         right--;
-                    }
-                    left++; 
-                    right--;
                 }
+                else if (sum < 0)
+                    left++;
+                else
+                    right--;
             }
+
         }
-        return temp;
-        // List <List<Integer>> ans = new ArrayList<>();
-        // for (List<Integer> li1: temp){
-        //     List<Integer> ans1 = new ArrayList<>();
-        //     for (int i : li1){
-        //         for (int j=0; j<n; j++){
-        //             if (i==nums[j] && !ans1.contains(j)){
-        //             ans1.add(j);
-        //             break;
-        //             }
-        //         }
+        return ans;
 
-        //     }
-        //     ans.add(ans1);
-        // }
-
-        // return ans;
     }
 }
