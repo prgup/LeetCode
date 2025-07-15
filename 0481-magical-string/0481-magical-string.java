4dca162fb13844863beb084bf38d6c3cec05ea80
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-07-16 01:42:40 +0530

    0481-MAGICAL-STRING Stats: Time: 3 ms (98.03%), Space: 42.4 MB (68.26%) - LeetHub

--- 0481-magical-string/0481-magical-string.java
+++ 0481-magical-string/0481-magical-string.java
@@ -1,23 +1,21 @@
 class Solution {
     public int magicalString(int n) {
-        int [] li = new int[n+2];
-        li[0]=1; li[1]=2; li[2]=2;
-        int ans=1;
-        int idxD=3, idxR=3;
-        for (idxR=3; idxR<n; idxR++){
-            int digit = 3-li[idxD-1];
-            int repeat = li[idxR-1];
-            while (repeat>0){
-                li[idxD]=digit;
-                idxD++;
-                repeat--;
-                if (digit==1)
-                ans++;
-                if (idxD>=n)
-                return ans;
-            }
-        }
-        return ans;
         
+        int[] magic = new int[n + 2];
+        magic[0] = 1; magic[1]=2; magic[2]=2;
+        int read = 2, write = 3;
+        int current = 1;
+        int countOnes = 1;
+
+        while (write < n) {
+            int repeat = magic[read++];
+            for (int i = 0; i < repeat && write < n; i++) {
+                magic[write++] = current;
+                if (current == 1) countOnes++;
+            }
+            current = 3 - current;
+        }
+
+        return countOnes;
     }
 }
