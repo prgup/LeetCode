4c519942fabd00ae4bbfab0b5f528c72312a06a8
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-07-27 21:28:09 +0530

    0416-PARTITION-EQUAL-SUBSET-SUM Stats: Time: 26 ms (85.25%), Space: 42.3 MB (81.68%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,30 +1,27 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int total = Arrays.stream(nums).sum();
-        if (total % 2 != 0) return false;
+        if (total % 2 != 0)
+            return false;
 
         int target = total / 2;
         int n = nums.length;
 
         // dp[i][j] = true if a sum j is possible using the first i numbers
-        boolean[][] dp = new boolean[n+1][target + 1];
+        boolean[] dp = new boolean[target + 1];
 
         // Base case: sum 0 is possible with any number of elements including zero
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
-        }
+        dp[0] = true;
 
         for (int i = 1; i <= n; i++) {
             int curr = nums[i - 1];
-            for (int j = 1; j <= target; j++) {
-                if (j >= curr) {
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j - curr];
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
+            for (int j = target; j >= curr; j--) {
+                //This ensures that each number is only used once per iteration.
+                // Prevents overwriting dp[j - num] before itâs used to compute dp[j].
+                dp[j] = dp[j] || dp[j - curr];
             }
         }
 
-        return dp[n][target];
+        return dp[target];
     }
 }
