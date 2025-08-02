6be8a917f83747b5fc826eeb5cf54d52b2f7945a
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-08-02 23:35:04 +0530

    0322-COIN-CHANGE Stats: Time: 27 ms (27.02%), Space: 45.1 MB (34.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,18 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int max = amount + 1; // acts like infinity
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, max);
-        dp[0] = 0; // base case
-
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i - coin >= 0) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        int n = coins.length;
+        int [][] dp = new int[n][amount+1];
+        Arrays.stream(dp).forEach(d -> Arrays.fill(d, amount+1));
+        
+        for (int i=0; i<n ; i++){
+            dp[i][0]=0;
+            int coin = coins[i];
+            for (int j=1; j<=amount; j++){
+                if (i>0){
+                    int old = dp[i-1][j];
+                    if (j>=coin) {
+                        int temp = Math.min(1+dp[i][j-coin], dp[i-1][j-coin]+1);
+                        old = Math.min(temp,old);
+                    }
+                    dp[i][j]= Math.min(old, dp[i][j]);
                 }
+                else if (j>=coin)
+                    dp[i][j]= Math.min(dp[i][j], 1+ dp[i][j-coin]);
             }
         }
-
-        return dp[amount] == max ? -1 : dp[amount];
+        return dp[n-1][amount]<(amount+1)?dp[n-1][amount]:-1;
+        
     }
 }
