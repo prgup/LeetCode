2c2bf820b75f2e8b8ca1eecb88dff41c34b2147c
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-08-05 01:37:33 +0530

    0494-TARGET-SUM Stats: Time: 6 ms (73.75%), Space: 45.2 MB (16.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,19 +1,26 @@
-class Solution {
+public class Solution {
+
     public int findTargetSumWays(int[] nums, int target) {
-        int sum = Arrays.stream(nums).sum();
-        if (Math.abs(target) > sum || (sum - target) % 2 != 0)
-            return 0;
-        return helper(nums, target, 0, 0);
+        int totalSum = Arrays.stream(nums).sum();
 
-    }
+        if (Math.abs(target)>totalSum || (totalSum-target)%2!=0)
+        return 0;
+        
+        int[] dp = new int[2 * totalSum + 1];
 
-    int helper(int[] nums, int target, int idx, int current) {
-        if (idx>=nums.length)
-        return target==current?1:0;
+        dp[ totalSum] = 1; 
+      
+        for (int index = 0; index < nums.length; index++) {
+            int[] next = new int[2 * totalSum + 1];
+            for (int sum = 0; sum <= 2*totalSum; sum++) {
+                if (dp[sum] > 0) {
+                    next[sum + nums[index] ] += dp[sum ];
+                    next[sum - nums[index] ] += dp[sum ];
+                }
+            }
+            dp = next;
+        }
 
-        int positive = helper(nums, target, idx+1, current+nums[idx]);
-        int negative = helper(nums, target, idx+1, current-nums[idx]);
-        return positive + negative;
-
+        return dp[target + totalSum];
     }
 }
