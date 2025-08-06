abd270275192a19d867ed3b4946e58ed8197a25f
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-08-07 01:49:18 +0530

    0128-LONGEST-CONSECUTIVE-SEQUENCE Stats: Time: 38 ms (16.84%), Space: 55.6 MB (99.53%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,21 +1,35 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Set <Integer> set = new HashSet<>();
-        for (int i: nums){
-            set.add(i);
-        }
-        int ans=0;
-        for (int i: set){
-            if (set.contains(i-1))
-            continue;
-            int cnt=1;
-            int j=1;
-            while (set.contains(j+i)){
-                j++;
-                cnt++;
-            }
-            ans= Math.max(ans, cnt);
-        }
-        return ans;
+
+        // Set<Integer> set = new HashSet<>();
+        // Arrays.stream(nums).forEach(i -> set.add(i));
+
+        // int startCount = (int) Arrays.stream(nums).filter(c-> !set.contains(c-1)).count();
+        // List<Integer> startElement = set.stream().filter(c -> !set.contains(c - 1)).collect(Collectors.toList());
+
+        // int ans = 0;
+        // for (int current : startElement) {
+        //     int tempCount = 0;
+        //     while (set.contains(current)) {
+        //         current++;
+        //         tempCount++;
+        //     }
+        //     ans = Math.max(tempCount, ans);
+        // }
+        // return ans;
+
+
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        return set.stream()
+                .filter(n -> !set.contains(n - 1))
+                .mapToInt(start -> {
+                    int count = 0;
+                    while (set.contains(start + count))
+                        count++;
+                    return count;
+                })
+                .max()
+                .orElse(0);
+
     }
 }
