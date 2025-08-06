8f673f9ea767fb5fcde819052a7a59bf3a52308a
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-08-07 00:13:28 +0530

    1250-LONGEST-COMMON-SUBSEQUENCE Stats: Time: 17 ms (91.23%), Space: 41.6 MB (96.91%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,19 +1,22 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        String input1="0"+text1;
-        String input2= "0"+text2;
+    public int longestCommonSubsequence(String input1, String input2) {
+        // String input1="0"+text1; //as they add O(n) tc and sc
+        // String input2= "0"+text2;
         int len1 = input1.length(), len2 = input2.length();
-        int [][] dp = new int [len1][len2];
-        for (int i=1; i<len1; i++){ //i=0 represet empty text1;
-            for (int j=1; j<len2; j++){ //j=0 represent empty text2;
-                if (input1.charAt(i)==input2.charAt(j)){
-                    dp[i][j]=1+dp[i-1][j-1];
+        int [] dp = new int [len2+1];
+        for (int i=1; i<len1+1; i++){ //i=0 represet empty text1;
+        int prev = dp[0]; //store dp[i-1]j-1];
+            for (int j=1; j<len2+1; j++){ //j=0 represent empty text2;
+                int temp = dp[j]; 
+                if (input1.charAt(i-1)==input2.charAt(j-1)){
+                    dp[j]=1+prev;
                 }
                 else 
-                    dp[i][j]= Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[j]= Math.max(dp[j], dp[j-1]);
+                prev=temp;
             }
         }
-        return dp[len1-1][len2-1];
+        return dp[len2];
         
     }
 }
