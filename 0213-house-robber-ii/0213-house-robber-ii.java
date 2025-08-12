017d1d66234ed8035b7f6119e286fe45953dd0ad
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-08-13 00:11:32 +0530

    0213-HOUSE-ROBBER-II Stats: Time: 0 ms (100%), Space: 40.9 MB (80.5%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,15 +1,23 @@
 class Solution {
+    int [] memoization;
     public int rob(int[] nums) {
-    if (nums.length == 1) return nums[0];
-    return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
-}
-    private int rob(int[] num, int lo, int hi) {
-    int include = 0, exclude = 0;
-    for (int j = lo; j <= hi; j++) {
-        int i = include, e = exclude;
-        include = e + num[j];
-        exclude = Math.max(e, i);
+        int n = nums.length;
+        if (n == 1)
+            return nums[0];
+        memoization = new int[n];
+        Arrays.fill(memoization,-1);
+        helper(nums, 0, n - 2);
+        int ans = memoization[0];
+        Arrays.fill(memoization,-1);
+        helper(nums, 1, n - 1);
+        return Math.max(memoization[1],ans);
     }
-    return Math.max(include, exclude);
-}
+
+    int helper(int[] nums, int idx, int end) {
+        if (idx > end)
+            return 0;
+        if (memoization[idx]==-1)
+        memoization[idx]= Math.max(helper(nums, idx + 1, end), nums[idx] + helper(nums, idx + 2, end));
+        return memoization[idx];
+    }
 }
