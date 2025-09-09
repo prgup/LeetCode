9b6f42c568224c33216f5bd16e7401af54bb6c9c
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-09-10 00:35:48 +0530

    0150-EVALUATE-REVERSE-POLISH-NOTATION Stats: Time: 5 ms (97.48%), Space: 44.3 MB (97.12%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -2,25 +2,19 @@ class Solution {
     public int evalRPN(String[] tokens) {
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         for (String token : tokens) {
-            if (token.matches("[*/+-]")) {
-                int b = stack.pop();
-                int a = stack.pop();
-                switch (token) {
-                    case "+":
-                        token = a + b + "";
-                        break;
-                    case "-":
-                        token = a - b + "";
-                        break;
-                    case "/":
-                        token = a / b + "";
-                        break;
-                    case "*":
-                        token = a * b + "";
-                        break;
+            switch (token) {
+                case "+" -> stack.push(stack.pop() + stack.pop());
+                case "-" -> {
+                    int b = stack.pop(), a = stack.pop();
+                    stack.push(a - b);
                 }
+                case "*" -> stack.push(stack.pop() * stack.pop());
+                case "/" -> {
+                    int b = stack.pop(), a = stack.pop();
+                    stack.push(a / b); // truncates toward zero, as required
+                }
+                default -> stack.push(Integer.parseInt(token));
             }
-            stack.push(Integer.parseInt(token));
         }
         return stack.pop();
     }
