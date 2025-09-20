28f71a282b6a79ccb60dda46ce59f00d82af7ed6
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-09-20 14:10:36 +0530

    0494-TARGET-SUM Stats: Time: 4 ms (91.13%), Space: 41.3 MB (88.05%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,26 +1,20 @@
-public class Solution {
-
+class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int totalSum = Arrays.stream(nums).sum();
+        int sum = Arrays.stream(nums).sum();
+        if ((target + sum) % 2 != 0 || Math.abs(target) > sum)
+            return 0;
+        return dpHelper(nums, (target + sum) / 2);
+    }
 
-        if (Math.abs(target)>totalSum || (totalSum-target)%2!=0)
-        return 0;
-        
-        int[] dp = new int[2 * totalSum + 1];
-
-        dp[ totalSum] = 1; 
-      
-        for (int index = 0; index < nums.length; index++) {
-            int[] next = new int[2 * totalSum + 1];
-            for (int sum = 0; sum <= 2*totalSum; sum++) {
-                if (dp[sum] > 0) {
-                    next[sum + nums[index] ] += dp[sum ];
-                    next[sum - nums[index] ] += dp[sum ];
-                }
+    int dpHelper(int[] nums, int target) {
+        int dp[] = new int[target + 1];
+        dp[0] = 1;
+        for (int i : nums) {
+            for (int j = target; j >= 0; j--) {
+                if (j >= i)
+                    dp[j] += dp[j - i];
             }
-            dp = next;
         }
-
-        return dp[target + totalSum];
+        return dp[target];
     }
 }
