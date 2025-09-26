acef4f3ee5958522dd13c2ce4ac2de87dca0ea2a
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-09-26 22:57:03 +0530

    0494-TARGET-SUM Stats: Time: 632 ms (15.89%), Space: 41.7 MB (63.68%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,20 +1,11 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int sum = Arrays.stream(nums).sum();
-        if ((target + sum) % 2 != 0 || Math.abs(target) > sum)
-            return 0;
-        return dpHelper(nums, (target + sum) / 2);
+        return helper(nums, target, 0);
     }
-
-    int dpHelper(int[] nums, int target) {
-        int dp[] = new int[target + 1];
-        dp[0] = 1;
-        for (int i : nums) {
-            for (int j = target; j >= 0; j--) {
-                if (j >= i)
-                    dp[j] += dp[j - i];
-            }
+    int helper (int [] nums, int target, int idx){
+        if (idx==nums.length){
+            return target==0?1:0;
         }
-        return dp[target];
+        return helper(nums, target-nums[idx], idx+1 ) + helper(nums, target+nums[idx], idx+1);
     }
 }
