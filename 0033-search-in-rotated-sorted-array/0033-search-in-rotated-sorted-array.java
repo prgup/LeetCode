533e809994488e68a09daf7b571db6b7d28691d2
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2025-10-14 22:26:40 +0530

    0033-SEARCH-IN-ROTATED-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 42.1 MB (69.8%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,32 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
+        int n = nums.length;
+        int left =0, right = n-1;
+        // int mid = (left+right)/2;
+        while (left <= right){
+            int mid = (left+right)/2;
+            if (nums[mid]==target)
+            return mid;
+            //left side is sorted
+            if (nums[left] <= nums[mid]){
+                if (target >= nums[left] && target < nums[mid])
+                right=mid-1;
+                else
+                left = mid+1;
 
-        //One side is always sorted. Always.
-        int left = 0, right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            if (nums[mid] == target) return mid;
-
-            // Left half is sorted
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1; // target is in the left half
-                } else {
-                    left = mid + 1;
-                }
             }
-            // Right half is sorted
+            //right side is sorted
             else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1; // target is in the right half
-                } else {
-                    right = mid - 1;
-                }
+                if (target > nums[mid] && target <= nums[right])
+                left=mid+1;
+                else
+                right = mid-1;
             }
+            
         }
-
+        // System.out.println(mid);
         return -1;
+        
     }
 }
