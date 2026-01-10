cdd1912106866d45e5e3e863ad4ae4eb5e18f466
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2026-01-11 02:08:57 +0530

    0712-MINIMUM-ASCII-DELETE-SUM-FOR-TWO-STRINGS Stats: Time: 21 ms (57.99%), Space: 46.6 MB (28.34%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -5,6 +5,14 @@ class Solution {
         int m = s2.length();
         dp = new int[n+1][m+1];
         Arrays.stream(dp).forEach(i -> Arrays.fill(i, -1));
+        dp[n][m]=0;
+        dp[n][m]=0;
+         for (int i = n-1; i >=0; i--) {
+            dp[i][m] = dp[i + 1][m] + s1.charAt(i);
+        }
+        for (int j =m-1; j >=0; j--) {
+            dp[n][j] = dp[n][j + 1] + s2.charAt(j);
+        }
         return count(s1, s2,dp,  0,0);
 
 
@@ -14,23 +22,6 @@ class Solution {
     int count( String s1, String s2, int [][] dp, int idx1, int idx2){
         if (dp[idx1][idx2]!=-1)
         return dp[idx1][idx2];
-        if (s1.length()<=idx1){
-            int ans=0;
-            for (int i=idx2; i<s2.length(); i++){
-                ans+=s2.charAt(i);
-            }
-            dp[idx1][idx2]=ans;
-            return ans;
-        }
-        if (s2.length() <= idx2){
-            int ans=0;
-            for (int i=idx1; i<s1.length(); i++){
-                ans+=s1.charAt(i);
-            }
-            dp[idx1][idx2]=ans;
-            return ans;
-
-        }
         if (s1.charAt(idx1)==s2.charAt(idx2)){
             dp[idx1][idx2]= count(s1, s2,dp,  idx1+1, idx2+1);
             return dp[idx1][idx2];
