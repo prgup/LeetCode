a8d12e1298b54cbc20ed115c214d2c982287f7b6
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2026-01-14 19:33:49 +0530

    1395-MINIMUM-TIME-VISITING-ALL-POINTS Stats: Time: 1 ms (95.81%), Space: 45.1 MB (21.13%) - LeetHub

--- 1395-minimum-time-visiting-all-points/1395-minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/1395-minimum-time-visiting-all-points.java
@@ -1,12 +1,20 @@
 class Solution {
-    public int minTimeToVisitAllPoints(int[][] p) {
-        int ans = 0;
-        for (int i = 1; i < p.length; i++) {
-            ans += Math.max(
-                Math.abs(p[i][0] - p[i - 1][0]),
-                Math.abs(p[i][1] - p[i - 1][1])
-            );
+    public int minTimeToVisitAllPoints(int[][] points) {
+
+        int x = points[0][0], y= points[0][1];
+        int ans =0;
+        for (int [] point: points){
+            int xDiff = Math.abs(x - point[0]);
+            int yDiff = Math.abs(y-point[1]);
+            ans+= Math.max(xDiff, yDiff);
+            x = point[0];
+            y= point[1];
         }
         return ans;
+
+
+
+
+        
     }
 }
