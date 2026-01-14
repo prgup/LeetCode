bae32c6a0861f9d482d8b4dfaa14a9dac077a14b
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2026-01-15 01:31:18 +0530

    0085-MAXIMAL-RECTANGLE Stats: Time: 11 ms (83.78%), Space: 49.7 MB (71.6%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,31 +1,26 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int maxArea =0;
+        int maxArea = 0;
         int n = matrix.length;
         int m = matrix[0].length;
-        int [][] heights = new int [n][m];
+        int[] heights = new int[m];
 
-        for (int i=0; i<n ;i++){
-            for (int j=0; j<m; j++){
-                int height = (i==0)?0:heights[i-1][j];
-                height = (matrix[i][j]=='1')?height +1:0;
-                heights[i][j] = height;
+        for (int j = 0; j < n; j++) {
+            int rowMaxArea = 0;
+            ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+            for (int i = 0; i < m; i++) {
+                heights[i] = (matrix[j][i] == '1') ? heights[i] + 1 : 0;
             }
-        }
 
-
-        for (int j=0; j<n; j++){
-            int rowMaxArea=0;
-            ArrayDeque <Integer> stack = new ArrayDeque<>();
-
-            for (int i=0; i<=m; i++){
-                int height = (i==m)?0:heights[j][i];
-                while (!stack.isEmpty() && heights[j][stack.peek()] > height){
-                    int h = heights[j][stack.pop()];
+            for (int i = 0; i <= m; i++) {
+                int height = (i == m) ? 0 : heights[i];
+                while (!stack.isEmpty() && heights[stack.peek()] > height) {
+                    int h = heights[stack.pop()];
                     int right = i;
-                    int left = (stack.isEmpty())?-1:stack.peek();
-                    int width = right-left-1;
-                    rowMaxArea=Math.max(rowMaxArea, width*h);
+                    int left = (stack.isEmpty()) ? -1 : stack.peek();
+                    int width = right - left - 1;
+                    rowMaxArea = Math.max(rowMaxArea, width * h);
                 }
                 stack.push(i);
             }
@@ -34,7 +29,5 @@ class Solution {
 
         return maxArea;
 
-
-        
     }
 }
