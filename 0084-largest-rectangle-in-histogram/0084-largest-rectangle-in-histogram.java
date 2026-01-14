34c70691b345834a7df605147deb31d2918700e6
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2026-01-14 20:38:30 +0530

    0084-LARGEST-RECTANGLE-IN-HISTOGRAM Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -3,23 +3,35 @@ class Solution {
         int n = heights.length;
         ArrayDeque <Integer> stack = new ArrayDeque<>();
         int ans = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        // int min = 
         for (int i=0; i<n ; i++){
-            if (stack.isEmpty() || heights[stack.peek()] <= heights[i]){
+            min = Math.min(min, heights[i]);
+            if (stack.isEmpty() || heights[stack.peekLast()] <= heights[i]){
+                // System.out.println(stack.peek());
                 stack.offer(i);
+                
+                // System.out.println(stack.toString()+ " in");
                 continue;
             }
-            while (!stack.isEmpty() && heights[stack.peek()] > heights[i]){
+            while (!stack.isEmpty() && heights[stack.peekLast()] > heights[i]){
                 int j = stack.pop();
                 int temp = (i-j)*heights[j];
+                // System.out.println(temp +" in");
                 ans = Math.max(temp, ans);
             }
+            stack.offer(i);
+            // System.out.println(stack.toString());
         }
         int i = n;
         while (!stack.isEmpty() ){
                 int j = stack.pop();
                 int temp = (i-j)*heights[j];
+                // System.out.println(temp);
                 ans = Math.max(temp, ans);
             }
+
+        ans= Math.max(ans, min*n);
 
 
         return ans;
