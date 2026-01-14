4f1c2e8ead54ca99b43e6cb75b7dcb293e265ba7
Praveen Gupta <72978292+prgup@users.noreply.github.com>
2026-01-14 23:32:40 +0530

    0084-LARGEST-RECTANGLE-IN-HISTOGRAM Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -4,9 +4,7 @@ class Solution {
         ArrayDeque <Integer> stack = new ArrayDeque<>();
         int ans = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        // int min = 
         for (int i=0; i<n ; i++){
-            min = Math.min(min, heights[i]);
             if (stack.isEmpty() || heights[stack.peekLast()] <= heights[i]){
                 // System.out.println(stack.peek());
                 stack.offer(i);
@@ -15,23 +13,33 @@ class Solution {
                 continue;
             }
             while (!stack.isEmpty() && heights[stack.peekLast()] > heights[i]){
-                int j = stack.pop();
-                int temp = (i-j)*heights[j];
-                // System.out.println(temp +" in");
+                int j = stack.pollLast();
+                int temp;
+                if (stack.isEmpty()){
+                    System.out.println(j + " "+ heights[j]);
+                    temp = (j+1)*heights[j];
+                }
+                else
+                    temp = (i-j)*heights[j];
                 ans = Math.max(temp, ans);
             }
             stack.offer(i);
-            // System.out.println(stack.toString());
         }
+        // System.out.println(stack.toString());
         int i = n;
         while (!stack.isEmpty() ){
-                int j = stack.pop();
-                int temp = (i-j)*heights[j];
-                // System.out.println(temp);
+                int j = stack.pollLast();
+                int temp;
+                if (stack.isEmpty()){
+                    // System.out.println(j + " "+ heights[j]);
+                    temp = n*heights[j];
+                }
+                else
+                    temp = (i-j)*heights[j];
+                
                 ans = Math.max(temp, ans);
             }
 
-        ans= Math.max(ans, min*n);
 
 
         return ans;
